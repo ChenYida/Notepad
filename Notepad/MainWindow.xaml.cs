8f1c3d1784bf2de87a2f1e9b0480b21566d20aed
Jeff <chen_yida@hotmail.com>
2019-07-11 03:12:47 +0800

    Add new feature.

--- Notepad/MainWindow.xaml.cs
+++ Notepad/MainWindow.xaml.cs
@@ -1,13 +1,54 @@
-﻿namespace Notepad
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Notepad
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
+        private string filePath;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            fileDialog.Title = "choose a txt";
+            fileDialog.Filter = "all files(*xls*)|*.txt*";
+            if (fileDialog.ShowDialog() != false)
+            {
+                filePath = fileDialog.FileName;
+                var tabItem = new TabItem() { Header = fileDialog.SafeFileName };
+                TabControl.Items.Add(tabItem);
+                var listBox = tabItem.Content as ListBox;
+                Read(filePath, listBox);
+                listBox.SelectionChanged += ListBox_SelectionChanged;
+                tabItem.IsSelected = true;
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Clipboard.SetDataObject((sender as ListBox).SelectedItem.ToString());
+        }
+
+        public void Read(string path, ListBox listBox)
+        {
+            listBox.Items.Clear();
+            using (StreamReader sr = new StreamReader(path, Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    listBox.Items.Add(line.ToString());
+                }
+            }
         }
     }
 }
